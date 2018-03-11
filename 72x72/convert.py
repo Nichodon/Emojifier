d061fb93dbc7cbd9a7566542f686293e329f7506
Nichodon <brandonsychung@gmail.com>
2018-03-11 11:10:03 -0700

    add thing

---
+++ 72x72/convert.py
@@ -0,0 +1,20 @@
+import json
+import os
+import os.path
+from pprint import pprint
+
+data = json.load(open('emojis.json'))
+
+n=-1
+while True:
+    try:
+        n += 1
+        if os.path.isfile(data["emojis"][n]["unicode"] + ".png"):
+            print "h"
+        if data["emojis"][n]["unicode"] != "":
+            print data["emojis"][n]["unicode"]
+            os.rename(data["emojis"][n]["unicode"] + ".png",
+                      str(n) + ".png")
+            print "success"
+    except WindowsError:
+        pass
